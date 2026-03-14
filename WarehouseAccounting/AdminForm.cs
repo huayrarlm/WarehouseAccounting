30cda300f5901498719b9080f96c0c20ca128bdc
huayrarlm <xytampo@gmail.com>
2026-03-14 10:15:09 +0300

    БД РАБОТАЕТ

---
+++ WarehouseAccounting/AdminForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WarehouseAccounting
+{
+    public partial class AdminForm : Form
+    {
+        private User _currentUser;
+
+        public AdminForm(User user)
+        {
+            InitializeComponent();
+            _currentUser = user;
+
+            // Отображаем имя пользователя в заголовке или на форме
+            this.Text = $"Администратор: {_currentUser.name}";
+        }
+    }
+}
